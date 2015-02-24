46112d1fc17d7d212b8650ac8f3ef6da7a224030
Clint Kelly <clint@wibidata.com>
2015-02-24 10:17:07 -0800

    import user metadata

--- src/main/scala/ImportRatings.scala
+++ src/main/scala/ImportRatings.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.joda.time
 import org.joda.time.DateTime
 
@@ -10,23 +11,9 @@ case class ReviewWithMetadata(userId: Long, reviewTime: DateTime, movieId: Long,
  * Import movie ratings and perform some denormalization.
  */
 object ImportRatings {
+  import com.datastax.spark.connector._
 
-  def main(args: Array[String]): Unit = {
-
-    // Set up SparkContext.
-    val conf = new SparkConf()
-      .setAppName("Simple Application")
-      .set("spark.cassandra.connection.host", "127.0.0.1")
-
-    val sc = new SparkContext(conf)
-
-    // Read user ratings.
-    // user_id movie_id rating timestamp
-    val userRatingsLines = sc.textFile("ml-100k/u.data")
-
-    // Read the movie metadata.
-    // movie_id movie_title release_date video_date url genres...
-    val movieMetadataLines = sc.textFile("ml-100k/u.item")
+  def writeDenormalizedRatings(userRatingsLines: RDD[String], movieMetadataLines: RDD[String]): Unit = {
 
     // Keys for both will be the movie IDs so that we can get the movie metadata.
     val userRatingsByMovieId = userRatingsLines.map { line: String =>
@@ -62,9 +49,54 @@ object ImportRatings {
     val firstFew = userRatingsWithMetadata.take(10)
     firstFew.foreach(println)
 
-    import com.datastax.spark.connector._
 
     userRatingsWithMetadata.saveToCassandra("movies", "reviews")
+  }
+
+  def writeUserMetadata(userMetadataLines: RDD[String]): Unit = {
+    // Pull out the fields and write to Cassandra as a user-defined type.
+    userMetadataLines.map { line =>
+      val tokens = line.split("\\|")
+      require(tokens.size == 5, s"Problem with $line")
+
+      val userId: Long = tokens(0).toLong
+
+      val userInfo = UDTValue.fromMap(Map(
+        "age" -> tokens(1).toInt,
+        "gender" -> tokens(2),
+        "occupation" -> tokens(3),
+        "zipcode" -> tokens(4)))
+      (userId, userInfo)
+    }.saveToCassandra("movies", "user_metadata", SomeColumns("user_id", "info"))
+  }
+
+
+  def main(args: Array[String]): Unit = {
+
+    // Set up SparkContext.
+    val conf = new SparkConf()
+      .setAppName("Simple Application")
+      .set("spark.cassandra.connection.host", "127.0.0.1")
+      .set("spark.cassandra.output.consistency.level", "LOCAL_ONE")
+
+    val sc = new SparkContext(conf)
+
+    // Read user ratings.
+    // user_id movie_id rating timestamp
+    val userRatingsLines = sc.textFile("ml-100k/u.data")
+    userRatingsLines.cache()
+
+    // Read the movie metadata.
+    // movie_id movie_title release_date video_date url genres...
+    val movieMetadataLines = sc.textFile("ml-100k/u.item")
+    movieMetadataLines.cache()
+
+    // Read the user metadata.
+    val userMetadataLines = sc.textFile("ml-100k/u.user")
+    userMetadataLines.cache()
+
+    writeDenormalizedRatings(userRatingsLines, movieMetadataLines)
+    writeUserMetadata(userMetadataLines)
 
   }
 
